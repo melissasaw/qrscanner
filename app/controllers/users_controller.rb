684bb1ebb2414d5333503af8da84458ea8b54853
Melissa Saw <melissasaw95@hotmail.com>
2019-01-18 16:42:37 +0800

    Created user log in, log out, with sessions, for controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+skip_before_action :verify_authenticity_token, :only => [:log_in]
 
 	def index
 	end
@@ -13,15 +14,49 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		byebug
 		user= User.new(get_params)
 
 		if user.save
 			redirect_to root_path
+			flash[:success] = "User successfully created! Log in to continue.."
 		else 
 			flash[:info] = "Whoopss, something went wrong. User was not created!"
 			render template: "/users/new"
 		end	
+	end
+
+	# Sign in section
+	def log_in
+		byebug
+		email = signin_params[:email]
+
+		if user = User.find_by(email: email)
+			# if email found, then authenticate password
+			input_password = signin_params[:password]
+			
+			if user.authenticate(input_password)
+				# create session
+				session[:user_id]=user.id
+				redirect_to root_path
+				flash[:success] = "Successful Log-in"
+			else
+				flash[:info] ="Wrong password. Please try again."
+			end
+
+		else
+			redirect_to root_path
+			flash[:info] = "Email not found. Please sign up."
+		end
+
+		# check existance of email
+		# authenticate password
+		# create sessions
+	end
+
+	def log_out
+		byebug
+		session[:user_id]=nil
+		redirect_to root_path
 	end
 
 	def show
@@ -40,6 +75,10 @@ private
 
   def get_params
   	params.require(:user).permit(:first_name, :last_name, :position, :email,:password)
+  end
+
+  def signin_params
+  	params.permit(:email, :password)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    
 	post 'create_qrcode', to: 'users#create_qrcode'
-
+	post 'log_in', to: 'users#log_in'
+	post 'log_out', to: 'users#log_out'
 	resources :users
 	root 'welcome#index'
 end
