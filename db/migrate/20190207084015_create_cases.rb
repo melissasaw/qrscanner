0099e9f0fa246e7ab79adaf5163670ea0eeb61f0
Melissa Saw <melissasaw95@hotmail.com>
2019-02-07 16:47:23 +0800

    Migrated columns, with serial number, case_type and t.ref to user

--- db/migrate/20190207084015_create_cases.rb
+++ db/migrate/20190207084015_create_cases.rb
@@ -1,7 +1,10 @@
 class CreateCases < ActiveRecord::Migration[5.2]
   def change
     create_table :cases do |t|
-
+    	t.string :serial_number
+    	t.integer :case_type
+    	t.references :user
+    
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_10_155500) do
+ActiveRecord::Schema.define(version: 2019_02_07_084015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cases", force: :cascade do |t|
+    t.string "serial_number"
+    t.integer "case_type"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cases_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
