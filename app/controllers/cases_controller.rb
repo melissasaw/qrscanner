eccb2670530ffbdf491328b02c6771686043b4ef
Melissa Saw <melissasaw95@hotmail.com>
2019-03-06 17:55:31 +0800

    Created cases and case show page

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -8,14 +8,21 @@ class CasesController < ApplicationController
 		# Given case type and serial number
 		@case=current_user.cases.new(get_params)
 		if @case.save
-			byebug
+			redirect_to case_path(@case)
 			#Generating QR code that carries the id of case with special word in front
-			@qr = RQRCode::QRCode.new("qrscannertest"+ @case.id.to_s, size: 4)
+			# @qr = RQRCode::QRCode.new("qrscannertest"+ @case.id.to_s, size: 4)
+			# Wanted to save qr as a string but doent work. 
+
 		else
 			flash[:info] = "Whoopss, something went wrong. Case was not created!"
 			render template: "/cases/new"
 		end
 
+	end
+
+	# Display page for each case
+	def show
+		@case=Case.find(params[:id])
 	end
 
 	def new 
