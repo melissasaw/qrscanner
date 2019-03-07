a09b5c7c141cada168a830234ff6bf476529ebb5
Melissa Saw <melissasaw95@hotmail.com>
2019-03-07 14:18:42 +0800

    User can create cases from user-show page, which serial number of case is automatically set in controller

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -8,7 +8,12 @@ class CasesController < ApplicationController
 		# Given case type and serial number
 		@case=current_user.cases.new(get_params)
 		if @case.save
+
+			# if case is save, only assign serial number.
+			@case.serial_number = "ORTHO"+ @case.case_type.to_s + "-" +@case.id.to_s
 			redirect_to case_path(@case)
+
+	
 			#Generating QR code that carries the id of case with special word in front
 			# @qr = RQRCode::QRCode.new("qrscannertest"+ @case.id.to_s, size: 4)
 			# Wanted to save qr as a string but doent work. 
